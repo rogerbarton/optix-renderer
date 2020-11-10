700a7cb631bbc608bb4dfc6f63f7353c6f782773
Matthias Busenhart <busenham@student.ethz.ch>
2020-11-10 20:36:02 +0100

    Fix uv coord

--- src/path_mats.cpp
+++ src/path_mats.cpp
@@ -14,7 +14,8 @@ public:
     Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray) const
     {
         Color3f Li = Color3f(0.f); // initial radiance§
-        Color3f t = Color3f(1.0);  // initial throughput
+        Color3f t = Color3f(1.f);  // initial throughput
+        Color3f bsdf_color = Color3f(0.f);
         Ray3f traceRay(ray);
         int counter = 0;
         while (true)
@@ -34,7 +35,7 @@ public:
             {
                 auto emitter = shape->getEmitter();
                 EmitterQueryRecord eqr(traceRay.o, its.p, its.shFrame.n);
-                Li = Li + t * emitter->eval(eqr);
+                Li += t * emitter->eval(eqr);
             }
 
             float succ_prob = std::min(t.maxCoeff(), 0.99f);
@@ -52,11 +53,14 @@ public:
             }
 
             BSDFQueryRecord bRec(its.toLocal(-traceRay.d));
+            bRec.uv = its.uv;
+            bRec.p = its.p;
+            bRec.measure = ESolidAngle;
 
             // Sample BSDF
-            Color3f bsdf_col = bsdf->sample(bRec, sampler->next2D());
+            bsdf_color = bsdf->sample(bRec, sampler->next2D());
 
-            t = t * bsdf_col;
+            t = t * bsdf_color;
 
             // create next ray to trace
             traceRay = Ray3f(its.p, its.toWorld(bRec.wo));
