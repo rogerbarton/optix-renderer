d3c06a603d844a2d2736bc8d78348aa44ccf6d19
Roger Barton <rbarton@student.ethz.ch>
2020-11-13 15:50:30 +0100

    openvdb working on windows

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -1,9 +1,9 @@
 call .\ext\vcpkg\bootstrap-vcpkg.bat
 
-:: call .\ext\vcpkg\vcpkg install zlib:x64-windows
+call .\ext\vcpkg\vcpkg install zlib:x64-windows
 call .\ext\vcpkg\vcpkg install blosc:x64-windows
-:: call .\ext\vcpkg\vcpkg install openexr:x64-windows
-:: call .\ext\vcpkg\vcpkg install tbb:x64-windows
+call .\ext\vcpkg\vcpkg install openexr:x64-windows
+call .\ext\vcpkg\vcpkg install tbb:x64-windows
 call .\ext\vcpkg\vcpkg install boost-iostreams:x64-windows
 call .\ext\vcpkg\vcpkg install boost-system:x64-windows
 call .\ext\vcpkg\vcpkg install boost-any:x64-windows
