7acaf1dd8186ea12b4aa8003a2019ea1b7645b72
Roger Barton <rbarton@student.ethz.ch>
2020-11-16 13:43:50 +0100

    clean volume

--- src/volume.cpp
+++ src/volume.cpp
@@ -1,199 +1,171 @@
-#include <iostream>
+#include <nori/volume.h>
 
 #include <openvdb/tools/LevelSetSphere.h>
 #include <nanovdb/util/OpenToNanoVDB.h>
-#include <nori/volume.h>
+
+#include <iostream>
 
 NORI_NAMESPACE_BEGIN
 
-Volume::Volume(const PropertyList &props)
-{
-	filename = getFileResolver()->resolve(props.getString("filename"));
-	if (!filename.exists())
-		throw NoriException(tfm::format("Volume: file not found %s", filename).c_str());
-
-	const auto originalExtension = filename.extension();
-
-	// NanoVDB has its own file format
-	// We cache the converted file, and update it if the original .vdb file has been touched
-	if (originalExtension == "vdb")
+	Volume::Volume(const PropertyList& props)
 	{
-		std::filesystem::path filenameStd{filename.str()};
-		std::filesystem::path filenameNvdb{filenameStd};
-		filenameNvdb.replace_extension(".nvdb");
+		filename = getFileResolver()->resolve(props.getString("filename"));
+		if (!filename.exists())
+			throw NoriException(tfm::format("Volume: file not found %s", filename).c_str());
 
-		// Check if cache exists and is up to date
-		if (!std::filesystem::exists(filenameNvdb) ||
-			std::filesystem::last_write_time(filenameStd) > std::filesystem::last_write_time(filenameNvdb))
+		const auto originalExtension = filename.extension();
+
+		// NanoVDB has its own file format
+		// We cache the converted file, and update it if the original .vdb file has been touched
+		if (originalExtension == "vdb")
 		{
-			std::cout << "Updating .nvdb cache..." << std::endl;
-			loadOpenVdbAndCacheNanoVdb(filenameNvdb);
+			std::filesystem::path filenameStd{filename.str()};
+			std::filesystem::path filenameNvdb{filenameStd};
+			filenameNvdb.replace_extension(".nvdb");
+
+			// Check if cache exists and is up to date
+			if (!std::filesystem::exists(filenameNvdb) ||
+			    std::filesystem::last_write_time(filenameStd) > std::filesystem::last_write_time(filenameNvdb))
+			{
+				std::cout << "Updating .nvdb cache..." << std::endl;
+				loadOpenVdbAndCacheNanoVdb(filenameNvdb);
+				loadNanoVdb();
+			}
+			else
+			{
+				std::cout << ".nvdb cache hit" << std::endl;
+				filename = filenameNvdb.string();
+				loadNanoVdb();
+			}
 		}
-		else
-		{
-			std::cout << ".nvdb cache hit" << std::endl;
-			filename = filenameNvdb.string();
+		else if (originalExtension == "nvdb")
 			loadNanoVdb();
-		}
-	}
-	else if (originalExtension == "nvdb")
-		loadNanoVdb();
-	else
-		throw NoriException("Volume: file extension .%s unknown.", originalExtension);
-}
-
-std::string Volume::toString() const
-{
-	return tfm::format("Volume[\n"
-					   "  filename = %s,\n"
-					   "]",
-					   filename);
-}
-
-/**
-		 * Loads a .nvdb (NanoVDB) file directly
-		 */
-void Volume::loadNanoVdb()
-{
-	densityHandle = nanovdb::io::readGrid(filename.str(), 0);
-	heatHandle = nanovdb::io::readGrid(filename.str(), 1);
-
-	{
-		const auto meta = densityHandle.gridMetaData();
-		std::cout << "\t" << meta->gridName() << std::endl;
-		std::cout << "\t grid type    : " << static_cast<int>(meta->gridType()) << std::endl;
-		std::cout << "\t grid class   : " << static_cast<int>(meta->gridClass()) << std::endl;
-
-		// Define a (raw) pointer to the NanoVDB grid on the host. Note we match the value type of the srcGrid!
-		grid = densityHandle.grid<float>();
-
-		if (!grid)
-			throw std::runtime_error("GridHandle does not contain a grid with value type float");
-
-		std::cout << "\t active voxels: " << static_cast<unsigned long>(grid->activeVoxelCount()) << std::endl;
-		std::cout << "\t voxel size   : " << grid->voxelSize()[0] << ", " << grid->voxelSize()[1] << ", "
-				  << grid->voxelSize()[2] << std::endl;
+		else
+			throw NoriException("Volume: file extension .%s unknown.", originalExtension);
 	}
 
+	std::string Volume::toString() const
 	{
-		const auto meta = heatHandle.gridMetaData();
-		std::cout << "\t" << meta->gridName() << std::endl;
-		std::cout << "\t grid type    : " << static_cast<int>(meta->gridType()) << std::endl;
-		std::cout << "\t grid class   : " << static_cast<int>(meta->gridClass()) << std::endl;
-
-		// Define a (raw) pointer to the NanoVDB grid on the host. Note we match the value type of the srcGrid!
-		heat = heatHandle.grid<float>();
-
-		if (!heat)
-			throw std::runtime_error("GridHandle does not contain a grid with value type float");
-
-		std::cout << "\t active voxels: " << static_cast<unsigned long>(heat->activeVoxelCount()) << std::endl;
-		std::cout << "\t voxel size   : " << heat->voxelSize()[0] << ", " << heat->voxelSize()[1] << ", "
-				  << heat->voxelSize()[2] << std::endl;
+		return tfm::format("Volume[\n"
+		                   "  filename = %s,\n"
+		                   "]",
+		                   filename);
 	}
 
-	// Get accessors for the two grids. Note that accessors only accelerate repeated access!
-	auto gridAcc = grid->getAccessor();
 
-	// Access and print out a cross-section of the narrow-band level set from the two grids
-	for (int i = 97; i < 104; ++i)
+	void Volume::readGrid(filesystem::path& file, uint64_t gridId, nanovdb::GridHandle<nanovdb::HostBuffer>& gridHandle,
+	                      nanovdb::NanoGrid<float>*& grid)
 	{
-		printf("(%3i,0,0) NanoVDB cpu: % -4.2f\n", i, gridAcc.getValue(nanovdb::Coord(i, 0, 0)));
-	}
-}
+		gridHandle = nanovdb::io::readGrid(filename.str(), gridId);
 
-/**
-		 * Loads a .vdb (OpenVDB) file and converts it to the NanoVDB format
-		 */
-void Volume::loadOpenVdbAndCacheNanoVdb(const std::filesystem::path &cacheFilename)
-{
-	try
-	{
-		openvdb::initialize();
-
-		openvdb::io::File file(filename.str());
-		file.open();
-		std::cout << "\tcompressed: " << std::boolalpha << file.hasBloscCompression() << std::endl;
-
-		openvdb::FloatGrid::Ptr densityGrid;
-		openvdb::FloatGrid::Ptr heatGrid;
-		for (openvdb::io::File::NameIterator nameIt = file.beginName(); nameIt != file.endName(); ++nameIt)
+		// -- Density
 		{
-			std::cout << "\t" << nameIt.gridName() << std::endl;
-			openvdb::GridBase::Ptr grid = file.readGrid(nameIt.gridName());
+			printGridMetaData(gridHandle);
 
-			std::cout << "\t grid type    : " << grid->type() << std::endl;
-			std::cout << "\t active voxels: " << grid->activeVoxelCount() << std::endl;
-			std::cout << "\t active dim   : " << grid->evalActiveVoxelDim() << std::endl;
-			std::cout << "\t voxel size   : " << grid->voxelSize() << std::endl;
+			// Define a (raw) pointer to the NanoVDB grid on the host. Note we match the value type of the srcGrid!
+			grid = gridHandle.grid<float>();
 
-			// Blender names: density, shadow, temperature, velocity
-			if (nameIt.gridName() == "density")
-				densityGrid = openvdb::gridPtrCast<openvdb::FloatGrid>(grid);
-			else if (nameIt.gridName() == "temperature")
-				heatGrid = openvdb::gridPtrCast<openvdb::FloatGrid>(grid);
+			if (!grid)
+				throw NoriException("GridHandle %i does not contain a grid with value type float. (%s)",
+				                    gridId, file.str());
+
+			printGridData(grid);
 		}
+	}
 
-		file.close();
-
-		// Create an OpenVDB grid (here a level set surface but replace this with your own code)
-		// auto srcGrid = openvdb::tools::createLevelSetSphere<openvdb::FloatGrid>(100.0f, openvdb::Vec3f(0.0f),
-		//                                                                         1.0f);
-
-		// Convert the OpenVDB grid, srcGrid, into a NanoVDB grid handle.
-		std::vector<nanovdb::GridHandle<nanovdb::HostBuffer>> gridHandles;
-		gridHandles.push_back(nanovdb::openToNanoVDB(*densityGrid));
-		gridHandles.push_back(nanovdb::openToNanoVDB(*heatGrid));
-
-		// Define a (raw) pointer to the NanoVDB grid on the host. Note we match the value type of the srcGrid!
-		grid = gridHandles[0].grid<float>();
-
-		if (!grid)
-			throw std::runtime_error("GridHandle does not contain a grid with value type float");
+	void Volume::loadNanoVdb()
+	{
+		readGrid(filename, 0, densityHandle, densityGrid);
+		readGrid(filename, 1, heatHandle, heatGrid);
 
 		// Get accessors for the two grids. Note that accessors only accelerate repeated access!
-		auto gridAcc = grid->getAccessor();
-		auto srcAcc = densityGrid->getAccessor();
+		auto densityAcc = densityGrid->getAccessor();
 
 		// Access and print out a cross-section of the narrow-band level set from the two grids
 		for (int i = 97; i < 104; ++i)
+			printf("(%3i,0,0) NanoVDB cpu: % -4.2f\n", i, densityAcc.getValue(nanovdb::Coord(i, 0, 0)));
+	}
+
+	void Volume::loadOpenVdbAndCacheNanoVdb(const std::filesystem::path& cacheFilename) const
+	{
+		try
 		{
-			printf("(%3i,0,0) OpenVDB cpu: % -4.2f, NanoVDB cpu: % -4.2f\n", i,
-				   srcAcc.getValue(openvdb::Coord(i, 0, 0)), gridAcc.getValue(nanovdb::Coord(i, 0, 0)));
-		}
+			// -- Read all grids from the OpenVDB .vdb file
+			openvdb::initialize();
+			openvdb::io::File file(filename.str());
+			file.open();
 
-		// Write the NanoVDB grid to file and throw if writing fails
-		std::cout << "Cache file: " << cacheFilename.string() << std::endl;
-		nanovdb::io::writeGrids<nanovdb::HostBuffer, std::vector>(cacheFilename.string(), gridHandles
+			openvdb::FloatGrid::Ptr              ovdbDensityGrid;
+			openvdb::FloatGrid::Ptr              ovdbHeatGrid;
+			for (openvdb::io::File::NameIterator nameIt = file.beginName(); nameIt != file.endName(); ++nameIt)
+			{
+				std::cout << "\t" << nameIt.gridName() << std::endl;
+				openvdb::GridBase::Ptr grid = file.readGrid(nameIt.gridName());
+
+				std::cout << "\t grid type    : " << grid->type() << std::endl;
+				std::cout << "\t active voxels: " << grid->activeVoxelCount() << std::endl;
+				std::cout << "\t active dim   : " << grid->evalActiveVoxelDim() << std::endl;
+				std::cout << "\t voxel size   : " << grid->voxelSize() << std::endl;
+
+				// Blender names: density, shadow, temperature, velocity
+				if (nameIt.gridName() == "density")
+					ovdbDensityGrid = openvdb::gridPtrCast<openvdb::FloatGrid>(grid);
+				else if (nameIt.gridName() == "temperature")
+					ovdbHeatGrid = openvdb::gridPtrCast<openvdb::FloatGrid>(grid);
+			}
+
+			file.close();
+			// -- end of OpenVDB part
+
+			// -- Convert OpenVDB to NanoVDB
+			// Convert the OpenVDB grid into a NanoVDB grid handle.
+			std::vector<nanovdb::GridHandle<nanovdb::HostBuffer>> gridHandles;
+			gridHandles.push_back(nanovdb::openToNanoVDB(*ovdbDensityGrid));
+			gridHandles.push_back(nanovdb::openToNanoVDB(*ovdbHeatGrid));
+
+			// Write the NanoVDB grid to file and throw if writing fails
+			nanovdb::io::writeGrids<nanovdb::HostBuffer, std::vector>(cacheFilename.string(), gridHandles
 #ifdef NANOVDB_USE_BLOSC
-																  ,
-																  nanovdb::io::Codec::BLOSC
+					, nanovdb::io::Codec::BLOSC
 #endif
-		);
+			);
 
-		openvdb::uninitialize();
+			openvdb::uninitialize();
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "loadOpenVdbAndCacheNanoVdb exception occurred: " << e.what() << std::endl;
+			openvdb::uninitialize();
+		}
 	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "An exception occurred: " << e.what() << std::endl;
-	}
-}
 
-/**
-		 * Writes the .nvdb (NanoVDB) file to disk. Use this to prevent formats converting each time.
-		 */
-void Volume::writeToNanoVdb(std::string filename)
-{
-	try
+	void Volume::writeToNanoVdb(const std::string& outfile) const
 	{
-		nanovdb::io::writeGrid(filename.c_str(), densityHandle);
+		try
+		{
+			nanovdb::io::writeGrid(outfile, densityHandle);
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << "Volume: writeToNanoVdb exception occurred: " << e.what() << std::endl;
+		}
 	}
-	catch (const std::exception &e)
-	{
-		std::cerr << "Volume: writeToNanoVdb exception occurred: " << e.what() << std::endl;
-	}
-}
 
-NORI_REGISTER_CLASS(Volume, "volume");
+	void Volume::printGridMetaData(const nanovdb::GridHandle<nanovdb::HostBuffer>& gridHandle)
+	{
+		const auto meta = gridHandle.gridMetaData();
+		std::cout << "\t" << meta->gridName() << std::endl;
+		std::cout << "\t grid type    : " << static_cast<int>(meta->gridType()) << std::endl;
+		std::cout << "\t grid class   : " << static_cast<int>(meta->gridClass()) << std::endl;
+	}
+
+	void Volume::printGridData(const nanovdb::NanoGrid<float>* grid)
+	{
+		std::cout << "\t active voxels: " << static_cast<unsigned long>(grid->activeVoxelCount()) << std::endl;
+		std::cout << "\t voxel size   : " << grid->voxelSize()[0] << ", " << grid->voxelSize()[1] << ", "
+		          << grid->voxelSize()[2] << std::endl;
+	}
+
+	NORI_REGISTER_CLASS(Volume, "volume");
 
 NORI_NAMESPACE_END
