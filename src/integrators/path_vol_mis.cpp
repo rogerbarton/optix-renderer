4c451e736f651554ac021343e0cc3db5df17917e
Roger Barton <rbarton@student.ethz.ch>
2020-12-10 15:46:44 +0100

    Copy path_vol_mats to path_vol_mis

--- src/integrators/path_vol_mis.cpp
+++ src/integrators/path_vol_mis.cpp
@@ -3,168 +3,135 @@
 #include <nori/sampler.h>
 #include <nori/scene.h>
 #include <nori/warp.h>
+#include <nori/phase.h>
 
 NORI_NAMESPACE_BEGIN
 
-/**
- * path_vol_mats but with importance sampling
- */
-class PathVolMISIntegrator : public Integrator
-{
-public:
-    explicit PathVolMISIntegrator(const PropertyList &propList) {}
-	NORI_OBJECT_DEFAULT_CLONE(PathVolMISIntegrator)
-	NORI_OBJECT_DEFAULT_UPDATE(PathVolMISIntegrator)
+	/**
+	 * path_vol_mats but with importance sampling
+	 */
+	class PathVolMISIntegrator : public Integrator
+	{
+	public:
+		explicit PathVolMISIntegrator(const PropertyList &propList) {}
+		NORI_OBJECT_DEFAULT_CLONE(PathVolMISIntegrator)
+		NORI_OBJECT_DEFAULT_UPDATE(PathVolMISIntegrator)
 
-    Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &ray, Color3f &albedo, Color3f &normal) const
-    {
-        Color3f Li = Color3f(0.f); // initial radiance
-        Color3f t = Color3f(1.0);  // initial throughput
+		Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &_ray, Color3f &albedo, Color3f &normal) const
+		{
+			Color3f Li         = Color3f(0.f);
+			Color3f throughput = Color3f(1.f);
 
-        Ray3f traceRay(ray);
-        int counter = 0;
-        float w_mats = 1.f;
-        float w_ems = 0.f;
+			// Start in ambient scene medium for now
+			const Medium *medium = scene->getAmbientMedium();
+			Ray3f        ray     = _ray;
 
-        while (true)
-        {
-            Color3f Li_EMS = Color3f(0.f);
-            float pdfems = 0.f, pdfmat = 0.f;
-            float pdfems_mats = 0.f;
-            float pdfmat_ems = 0.f;
+			int bounces = 0;
+			while (true)
+			{
+				Intersection its;
+				if (!scene->rayIntersect(ray, its))
+				{
+					if (scene->getEnvMap())
+					{
+						EmitterQueryRecord eqr(ray.d);
+						Li += throughput * scene->getEnvMap()->eval(eqr);
+					}
+					break; // miss scene
 
-            Intersection its;
-            if (!scene->rayIntersect(traceRay, its))
-            {
+				}
 
-                if (scene->getEnvMap())
-                {
-                    EmitterQueryRecord eqr;
-                    eqr.wi = traceRay.d;
-                    Li += t * scene->getEnvMap()->eval(eqr);
-                }
-                break;
-            }
+				// -- Find next interaction point
+				MediumQueryRecord mRec(ray);
+				const float       tm                  = medium->sampleFreePath(mRec, sampler->next1D());
+				const bool        isMediumInteraction = tm < its.t;
 
-            // get colliding object and shape
-            const Shape *shape = its.shape;
-            const BSDF *bsdf = shape->getBSDF();
+				Vector3f p = isMediumInteraction ? ray(tm) : its.p;
 
-            // if shape is emitter, add eval to result
-	        if (shape->getEmitter())
-            {
-                auto emitter = shape->getEmitter();
-                EmitterQueryRecord eqr(traceRay.o, its.p, its.shFrame.n);
-                Li += w_mats * t * emitter->eval(eqr);
-            }
+				Color3f Tr = medium->getTransmittance(ray.o, p);
+				throughput *= Tr;
 
-            float succ_prob = std::min(t.maxCoeff(), 0.99f);
-            succ_prob = std::max(succ_prob, Epsilon);
-            if (counter < 0)
-            {
-                counter++;
-            }
-            else if (sampler->next1D() > succ_prob)
-            {
-                break;
-            }
-            else
-            {
-                t /= succ_prob;
-            }
+				// -- Apply captured emission
+				if (isMediumInteraction)
+				{
+					if (medium->getEmitter())
+					{
+						EmitterQueryRecord lRec{ray.o, its.p, 0.f};
+						Li += throughput * medium->getEmitter()->eval(lRec);
+					}
+				}
+				else
+				{
+					if (its.shape->getEmitter())
+					{
+						EmitterQueryRecord lRec{ray.o, its.p, its.shFrame.n};
+						Li += throughput * its.shape->getEmitter()->eval(lRec);
+					}
+				}
 
-            // WE NEED probs seen from its.p
-            // ========== EMS =============
-            const Emitter *emitter = scene->getRandomEmitter(sampler->next1D());
-            if (!emitter)
-            {
-                throw NoriException("No Emitter in scene!");
-            }
+				// -- Russian roulette with success probability
+				{
+					const float rouletteSuccess = std::min(throughput.maxCoeff(), 0.99f);
 
-            EmitterQueryRecord eqr_ems(its.p);
-            // sample the emitter
-            Color3f ems_col = emitter->sample(eqr_ems, sampler->next2D());
+					if (sampler->next1D() > rouletteSuccess || rouletteSuccess < Epsilon)
+						break;
+					// Adjust throughput in case of survival
+					throughput /= rouletteSuccess;
+				}
 
-            Vector3f we = its.toLocal(eqr_ems.wi);
+				// -- Determine next ray
+				Vector3f wo = ray.d; // default is unperturbed ray
+				if (isMediumInteraction)
+				{
+					// Next interaction is in medium => Sample phase
+					PhaseQueryRecord pRec(its.toLocal(-ray.d));
+					// TODO: how to adjust throughput?
+					throughput *= medium->getPhase()->sample(pRec, sampler->next2D());
+					wo = its.toWorld(pRec.wo);
+				}
+				else
+				{
+					// Next interaction is on a surface => sample surface
+					if (its.shape->getBSDF())
+					{
+						// Surface interaction
+						const BSDF      *bsdf = its.shape->getBSDF();
+						BSDFQueryRecord bRec{its.toLocal(-ray.d)};
+						bRec.uv = its.uv;
 
-            if (!ems_col.isZero(Epsilon))
-            {
-                if (!scene->rayIntersect(eqr_ems.shadowRay))
-                {
-                    BSDFQueryRecord bqr_ems(its.toLocal(-traceRay.d), we, EMeasure::ESolidAngle);
-                    bqr_ems.uv = its.uv;
-                    bqr_ems.p = its.p;
-                    Color3f bsdf_col_ems = bsdf->eval(bqr_ems);
+						throughput *= bsdf->sample(bRec, sampler->next2D());
+						// Note w_i . n is the cosTheta term included in the sample function
+						wo      = its.toWorld(bRec.wo);
+					}
 
-                    float cos = Frame::cosTheta(we);
+					// Update current medium, if entering or leaving shape (note: overlaps unhandled)
+					if (ray.d.dot(wo) > 0) // change in shape, else reflected
+						medium = wo.dot(its.geoFrame.n) < 0.f && its.shape->getMedium() ?
+						         its.shape->getMedium() :     // entering
+						         scene->getAmbientMedium();   // leaving
+				}
 
-                    Li_EMS = ems_col * cos * bsdf_col_ems * scene->getLights().size();
-                    pdfems_mats = bsdf->pdf(bqr_ems);
-                    pdfems = emitter->pdf(eqr_ems) / scene->getLights().size();
-                }
-            }
-            if ((pdfems_mats + pdfems) > Epsilon)
-            {
-                w_ems = pdfems / (pdfems_mats + pdfems);
-            }
+				ray = Ray3f(p, wo);
 
-            // ========== MATS =============
-            BSDFQueryRecord bRec_MATS(its.toLocal(-traceRay.d));
-            bRec_MATS.uv = its.uv;
-            bRec_MATS.p = its.p;
-            // Sample BSDF
-            Color3f bsdf_col = bsdf->sample(bRec_MATS, sampler->next2D());
+				bounces++;
+			}
 
-            if (!bsdf_col.isZero(Epsilon))
-            {
-                Ray3f shadowray(its.p, its.toWorld(bRec_MATS.wo));
-                Intersection itsS;
-                if (scene->rayIntersect(shadowray, itsS))
-                {
-	                if (itsS.shape->getEmitter())
-                    {
-                        EmitterQueryRecord eqr_mats(its.p, itsS.p, itsS.shFrame.n);
-                        pdfmat = bsdf->pdf(bRec_MATS);
-                        pdfmat_ems = itsS.shape->getEmitter()->pdf(eqr_mats) / scene->getLights().size();
-
-                        if ((pdfmat + pdfmat_ems) > Epsilon)
-                        {
-                            w_mats = pdfmat / (pdfmat + pdfmat_ems);
-                        }
-                    }
-                }
-            }
-
-            // check for discrete bsdf
-            if (bRec_MATS.measure == EDiscrete)
-            {
-                w_ems = 0.f;
-                w_mats = 1.f;
-            }
-
-            Li += w_ems * t * Li_EMS;
-            t = t * bsdf_col;
-
-            // create next ray to trace
-            traceRay = Ray3f(its.p, its.toWorld(bRec_MATS.wo));
-        }
-
-        return Li;
-    }
-    std::string toString() const
-    {
-        return "PathVolMISIntegrator[]";
-    }
+			return Li;
+		}
+		std::string toString() const
+		{
+			return "PathVolMISIntegrator[]";
+		}
 #ifdef NORI_USE_IMGUI
-	NORI_OBJECT_IMGUI_NAME("Path MIS w/ Volumes");
-    virtual bool getImGuiNodes() override { return Integrator::getImGuiNodes(); }
+		NORI_OBJECT_IMGUI_NAME("Path MIS w/ Volumes");
+		virtual bool getImGuiNodes() override { return Integrator::getImGuiNodes(); }
 #endif
 
 #ifdef NORI_USE_OPTIX
 		IntegratorType getOptixIntegratorType() const override { return INTEGRATOR_TYPE_PATH_MIS; }
 #endif
-};
+	};
 
-NORI_REGISTER_CLASS(PathVolMISIntegrator, "path_vol_mis");
+	NORI_REGISTER_CLASS(PathVolMISIntegrator, "path_vol_mis");
 
 NORI_NAMESPACE_END
