60ab86b6680712a1dd68d5569aab5cf2205ba24b
Roger Barton <rbarton@student.ethz.ch>
2020-12-12 15:03:26 +0100

    - cuda print_pixel

--- include/nori/optix/cuda/sutil/exception.h
+++ include/nori/optix/cuda/sutil/exception.h
@@ -5,3 +5,17 @@
 #pragma once
 
 #define ERROR_COLOR make_float3(255,192,203)
+
+#define IF_PIXEL( x_, y_ )                                                     \
+    const uint3 launch_idx__ = optixGetLaunchIndex();                          \
+    if( launch_idx__.x == (x_) && launch_idx__.y == (y_) )                     \
+
+#define PRINT_PIXEL( x_, y_, str, ... )                                        \
+do                                                                             \
+{                                                                              \
+    const uint3 launch_idx = optixGetLaunchIndex();                            \
+    if( launch_idx.x == (x_) && launch_idx.y == (y_) )                         \
+    {                                                                          \
+         printf( str, __VA_ARGS__  );                                          \
+    }                                                                          \
+} while(0);
