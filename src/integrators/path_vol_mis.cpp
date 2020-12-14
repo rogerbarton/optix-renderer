b24c75353e219ba2af937032b372e6d5ef97b77c
Roger Barton <rbarton@student.ethz.ch>
2020-12-14 13:38:15 +0100

    path vol mis, extract methods, clean first mats part

--- src/integrators/path_vol_mis.cpp
+++ src/integrators/path_vol_mis.cpp
@@ -17,6 +17,81 @@ NORI_NAMESPACE_BEGIN
 		NORI_OBJECT_DEFAULT_CLONE(PathVolMISIntegrator)
 		NORI_OBJECT_DEFAULT_UPDATE(PathVolMISIntegrator)
 
+		/**
+		 * Propagate shadowray through media until we hit a surface with a bsdf
+		 * @param shadowRay
+		 * @param shadowTr The transmittance along the shadowray
+		 * @return If the ray is occluded
+		 */
+		bool traceShadowray(const Scene *scene, Sampler *sampler, const Medium *shadowMedium, Ray3f shadowRay,
+		                    Color3f &shadowTr) const
+		{
+			Intersection shadowIts;
+			while (scene->rayIntersect(shadowRay, shadowIts))
+			{
+				if (shadowIts.shape->getBSDF())
+					return true;
+
+				shadowTr *= shadowMedium->getTransmittance(shadowRay.o, shadowIts.p, false, *sampler);
+
+				shadowMedium =
+						shadowRay.d.dot(shadowIts.geoFrame.n) < 0.f && shadowIts.shape->getMedium() ?
+						shadowIts.shape->getMedium() :     // entering
+						scene->getAmbientMedium();   // leaving
+
+				shadowRay.o = shadowIts.p; // Note: no normal maps applied to keep ray.d constant
+				shadowRay.maxt -= shadowIts.t;
+			}
+
+			return false;
+		}
+
+		/**
+		 * EMS - Sample random emitter and calculate its pdf for the weight
+		 * @return Li received from emitter
+		 */
+		Color3f
+		sampleEmitter(const Scene *scene, Sampler *sampler, const Color3f &throughput, const Medium *medium,
+		              const Ray3f &ray, const Intersection &its, const BSDF *bsdf, const BSDFQueryRecord &bRec) const
+		{
+			if (bRec.measure != ESolidAngle) // skip delta bsdf's
+				return 0.f;
+
+			const auto &lights        = scene->getLights();
+			const auto emitter        = scene->getRandomEmitter(sampler->next1D());
+
+			EmitterQueryRecord lRec{its.p};
+			const Color3f      Le     = emitter->sample(lRec, sampler->next2D()) * lights.size();
+			const float        pdf_em = lRec.pdf / lights.size();
+
+			// shadowRay data
+			Ray3f        shadowRay = lRec.shadowRay;
+			Color3f      shadowTr  = 1.f;
+			const Medium *shadowMedium;
+			if (ray.d.dot(shadowRay.d) > 0) // change in shape, else reflected
+				shadowMedium = shadowRay.d.dot(its.geoFrame.n) < 0.f && its.shape->getMedium() ?
+				               its.shape->getMedium() :     // entering
+				               scene->getAmbientMedium();   // leaving
+			else
+				shadowMedium = medium;
+
+			if (!traceShadowray(scene, sampler, shadowMedium, shadowRay, shadowTr))
+			{
+				// Use same measure as the other bRec we sampled (its the same bsdf)
+				BSDFQueryRecord bRecEm{its.toLocal(-ray.d), its.toLocal(shadowRay.d), bRec.measure};
+				bRecEm.uv = its.uv;
+
+				const float pdf_em_mat = bsdf->pdf(bRecEm);
+				const float w_em       = pdf_em > Epsilon ? pdf_em / (pdf_em + pdf_em_mat) : 0.f;
+
+				if (w_em > Epsilon)
+					return w_em * shadowTr * throughput *
+					       abs(lRec.wi.dot(its.shFrame.n)) * bsdf->eval(bRecEm) * Le;
+			}
+
+			return 0.f;
+		}
+
 		Color3f Li(const Scene *scene, Sampler *sampler, const Ray3f &_ray, Color3f &albedo, Color3f &normal) const
 		{
 			Color3f Li         = Color3f(0.f);
@@ -34,8 +109,10 @@ NORI_NAMESPACE_BEGIN
 			while (true)
 			{
 				Intersection its;
-				if (!scene->rayIntersect(ray, its)) {
-					if(scene->getEnvMap()) {
+				if (!scene->rayIntersect(ray, its))
+				{
+					if (scene->getEnvMap())
+					{
 						EmitterQueryRecord eqr;
 						eqr.wi = ray.d;
 						Li += throughput * scene->getEnvMap()->eval(eqr);
@@ -45,40 +122,42 @@ NORI_NAMESPACE_BEGIN
 
 				// -- Find next interaction point
 				MediumQueryRecord mRec(ray);
-				const float       tm                  = medium->sampleFreePath(mRec, *sampler);
-				const bool        isMediumInteraction = tm < its.t;
+				const float       tm = medium->sampleFreePath(mRec, *sampler);
 
-				Point3f p = isMediumInteraction ? ray(tm) : its.p;
+				const bool isMediumInteraction = tm < its.t;
+				Point3f    p                   = isMediumInteraction ? ray(tm) : its.p;
 
+				// Factor in medium transmittance up to interaction point p
 				Color3f Tr = medium->getTransmittance(ray.o, p, isMediumInteraction, *sampler);
 				throughput *= Tr;
 
-				// -- Apply captured emission
+				// -- Apply captured emission - MATS
+				const Emitter      *hitEmitter = nullptr;
+				EmitterQueryRecord lRec;
 				if (isMediumInteraction)
 				{
 					if (medium->getEmitter())
 					{
-						EmitterQueryRecord lRec{ray.o, its.p, 0.f};
-
-						const float pdf_mat_em = medium->getEmitter()->pdf(lRec) / lights.size();
-						// TODO: use pdf_medium
-						const float w_mat      = pdf_mat > Epsilon ? pdf_mat / (pdf_mat + pdf_mat_em) : 0.f;
-
-						Li += w_mat * throughput * medium->getEmitter()->eval(lRec);
+						hitEmitter = medium->getEmitter();
+						lRec       = EmitterQueryRecord{ray.o, its.p, 0.f};
 					}
 				}
 				else
 				{
 					if (its.shape->getEmitter())
 					{
-						EmitterQueryRecord lRec{ray.o, its.p, its.shFrame.n};
-
-						const float pdf_mat_em = its.shape->getEmitter()->pdf(lRec) / lights.size();
-						const float w_mat      = pdf_mat_measure == EDiscrete ? 1.f :
-						                         pdf_mat > Epsilon ? pdf_mat / (pdf_mat + pdf_mat_em) : 0.f;
-
-						Li += w_mat * throughput * its.shape->getEmitter()->eval(lRec);
+						hitEmitter = its.shape->getEmitter();
+						lRec       = EmitterQueryRecord{ray.o, its.p, its.shFrame.n};
 					}
+				}
+
+				if (hitEmitter)
+				{
+					const float pdf_mat_em = hitEmitter->pdf(lRec) / lights.size();
+					const float w_mat      = pdf_mat_measure == EDiscrete ? 1.f :
+					                         pdf_mat > Epsilon ? pdf_mat / (pdf_mat + pdf_mat_em) : 0.f;
+
+					Li += w_mat * throughput * hitEmitter->eval(lRec);
 				}
 
 				// -- Russian roulette with success probability
@@ -120,63 +199,7 @@ NORI_NAMESPACE_BEGIN
 						pdf_mat         = bsdf->pdf(bRec);
 						pdf_mat_measure = bRec.measure;
 
-						if (bRec.measure == ESolidAngle) // skip delta bsdf's
-						{
-							// sample random emitter and calculate its pdf for the weight
-							const auto emitter        = scene->getRandomEmitter(sampler->next1D());
-
-							EmitterQueryRecord lRec{its.p};
-							const Color3f      Le     = emitter->sample(lRec, sampler->next2D()) * lights.size();
-							const float        pdf_em = lRec.pdf / lights.size();
-
-							// shadowRay data
-							Ray3f        shadowRay = lRec.shadowRay;
-							Color3f      shadowTr  = 1.f;
-							bool         occluded  = false;
-							const Medium *shadowMedium;
-							if (ray.d.dot(shadowRay.d) > 0) // change in shape, else reflected
-								shadowMedium = shadowRay.d.dot(its.geoFrame.n) < 0.f && its.shape->getMedium() ?
-								               its.shape->getMedium() :     // entering
-								               scene->getAmbientMedium();   // leaving
-							else
-								shadowMedium = medium;
-
-							// Propagate shadowray through media until we hit a surface with a bsdf
-							while (true)
-							{
-								Intersection shadowIts;
-								if (!scene->rayIntersect(shadowRay, shadowIts))
-									break;
-								if (shadowIts.shape->getBSDF())
-								{
-									occluded = true;
-									break;
-								}
-								shadowTr *= shadowMedium->getTransmittance(shadowRay.o, shadowIts.p, false,
-								                                           *sampler);
-								shadowMedium =
-										shadowRay.d.dot(shadowIts.geoFrame.n) < 0.f && shadowIts.shape->getMedium() ?
-										shadowIts.shape->getMedium() :     // entering
-										scene->getAmbientMedium();   // leaving
-
-								shadowRay.o = shadowIts.p; // Note: no normal maps applied to keep ray.d constant
-								shadowRay.maxt -= shadowIts.t;
-							}
-
-							if (!occluded)
-							{
-								// Use same measure as the other bRec we sampled (its the same bsdf)
-								BSDFQueryRecord bRecEm{its.toLocal(-ray.d), its.toLocal(shadowRay.d), bRec.measure};
-								bRecEm.uv = its.uv;
-
-								const float pdf_em_mat = bsdf->pdf(bRecEm);
-								const float w_em       = pdf_em > Epsilon ? pdf_em / (pdf_em + pdf_em_mat) : 0.f;
-
-								if (w_em > Epsilon)
-									Li += w_em * shadowTr * throughput *
-									      abs(lRec.wi.dot(its.shFrame.n)) * bsdf->eval(bRecEm) * Le;
-							}
-						} // End Emitter Sampling
+						Li += sampleEmitter(scene, sampler, throughput, medium, ray, its, bsdf, bRec);
 
 						throughput *= bsdfSample;
 					}
