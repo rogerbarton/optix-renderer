048b5907c26d1146407b9f353c02aefffe32f1df
Roger Barton <rbarton@student.ethz.ch>
2020-12-16 18:38:30 +0100

    Move sampleRay to shaders/camera.h, handle different camera types

--- include/nori/optix/cuda/raygen.cpp
+++ include/nori/optix/cuda/raygen.cpp
@@ -11,16 +11,17 @@
 
 #include "RadiancePrd.h"
 
+#include "shaders/camera.h"
+
 #include "sutil/exception.h"
 #include "sutil/helpers.h"
 #include "sutil/vec_math.h"
 #include "sutil/random.h"
 #include "sutil/warp.h"
 
-static __forceinline__ __device__ void sampleRay(
-		unsigned int &seed,
-		float3 &ray_direction,
-		float3 &ray_origin);
+extern "C" {
+__constant__ LaunchParams launchParams;
+}
 
 static __forceinline__ __device__ void traceRadiance(
 		OptixTraversableHandle handle,
@@ -28,17 +29,47 @@ static __forceinline__ __device__ void traceRadiance(
 		float3 ray_direction,
 		float tmin,
 		float tmax,
-		RadiancePrd *prd);
+		RadiancePrd *prd)
+{
+	unsigned int u0, u1;
+	packPointer(prd, u0, u1);
+	optixTrace(
+			handle,
+			ray_origin,
+			ray_direction,
+			tmin,
+			tmax,
+			0.0f,                        // rayTime
+			OptixVisibilityMask(1),
+			OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT,
+			RAY_TYPE_RADIANCE,          // SBT offset
+			RAY_TYPE_COUNT,             // SBT stride
+			RAY_TYPE_RADIANCE,       // missSBTIndex
+			u0, u1);
+}
 
 static __forceinline__ __device__ bool traceShadowray(
 		OptixTraversableHandle handle,
 		float3 ray_origin,
 		float3 ray_direction,
 		float tmin,
-		float tmax);
-
-extern "C" {
-__constant__ LaunchParams launchParams;
+		float tmax)
+{
+	unsigned int occluded = 0u;
+	optixTrace(
+			handle,
+			ray_origin,
+			ray_direction,
+			tmin,
+			tmax,
+			0.0f,                        // rayTime
+			OptixVisibilityMask(1),
+			OPTIX_RAY_FLAG_NONE,
+			RAY_TYPE_SHADOWRAY,         // SBT offset
+			RAY_TYPE_COUNT,             // SBT stride
+			RAY_TYPE_SHADOWRAY,      // missSBTIndex
+			occluded);
+	return occluded;
 }
 
 extern "C" __global__ void __raygen__perspective()
@@ -57,7 +88,7 @@ extern "C" __global__ void __raygen__perspective()
 	{
 		float3 rayDirection;
 		float3 rayOrigin;
-		sampleRay(seed, rayDirection, rayOrigin);
+		sampleRay(launchParams, seed, rayOrigin, rayDirection);
 
 		// -- Integrator::Li()
 		RadiancePrd prd{};
@@ -99,9 +130,12 @@ extern "C" __global__ void __raygen__perspective()
 
 		// Store Li
 		color += prd.Li;
-		albedo += prd.albedo;
+		// albedo += prd.albedo;
 		normal += prd.normal;
+
+		albedo += rayDirection;
 	}
+
 
 	// PRINT_PIXEL(100, 100, "color  (%f, %f, %f), ", color.x, color.y, color.z);
 	// PRINT_PIXEL(100, 100, "albedo (%f, %f, %f), ", albedo.x, albedo.y, albedo.z);
@@ -114,93 +148,4 @@ extern "C" __global__ void __raygen__perspective()
 	                            launchParams.d_outputBufferAlbedo, albedo);
 	interpolateAndApplyToBuffer(launchParams.sampleIndex, launchParams.samplesPerLaunch, pixelIdx,
 	                            launchParams.d_outputBufferNormal, normal);
-}
-
-static __forceinline__ __device__ void sampleRay(
-		unsigned int &seed,
-		float3 &ray_direction,
-		float3 &ray_origin)
-{
-	const unsigned int w   = launchParams.imageWidth;
-	const unsigned int h   = launchParams.imageHeight;
-	const float3       eye = launchParams.camera.eye;
-	const float3       U   = launchParams.camera.U;
-	const float3       V   = launchParams.camera.V;
-	const float3       W   = launchParams.camera.W;
-	const uint3        idx = optixGetLaunchIndex();
-
-	const float2 subpixel_jitter = make_float2(rnd(seed), rnd(seed));
-
-	// Local ray
-	float3 o = {0, 0, 0};
-	float3 d = 2.0f * make_float3(
-			(static_cast<float>( idx.x ) + subpixel_jitter.x) / static_cast<float>( w ),
-			(static_cast<float>( idx.y ) + subpixel_jitter.y) / static_cast<float>( h ),
-			0.5f
-	) - 1.0f;
-
-	if (launchParams.camera.lensRadius > EPSILON)
-	{
-		const float2 pLens  = launchParams.camera.lensRadius *
-		                      squareToUniformDisk(make_float2(rnd(seed), rnd(seed)));
-		const float  ft     = launchParams.camera.focalDistance / d.z;
-		const float3 pFocus = o + d * ft;
-
-		o = make_float3(pLens.x, pLens.y, 0.f);
-		// direction connecting aperture and focal plane points
-		d = normalize(pFocus - o);
-	}
-
-	// Transform camera to world
-	ray_direction = normalize(d.x * U + d.y * V + d.z * W);
-	ray_origin    = eye + o.x * U + o.y * V + o.z * W;
-}
-
-static __forceinline__ __device__ void traceRadiance(
-		OptixTraversableHandle handle,
-		float3 ray_origin,
-		float3 ray_direction,
-		float tmin,
-		float tmax,
-		RadiancePrd *prd)
-{
-	unsigned int u0, u1;
-	packPointer(prd, u0, u1);
-	optixTrace(
-			handle,
-			ray_origin,
-			ray_direction,
-			tmin,
-			tmax,
-			0.0f,                        // rayTime
-			OptixVisibilityMask(1),
-			OPTIX_RAY_FLAG_NONE,
-			RAY_TYPE_RADIANCE,          // SBT offset
-			RAY_TYPE_COUNT,             // SBT stride
-			RAY_TYPE_RADIANCE,       // missSBTIndex
-			u0, u1);
-}
-
-static __forceinline__ __device__ bool traceShadowray(
-		OptixTraversableHandle handle,
-		float3 ray_origin,
-		float3 ray_direction,
-		float tmin,
-		float tmax)
-{
-	unsigned int occluded = 0u;
-	optixTrace(
-			handle,
-			ray_origin,
-			ray_direction,
-			tmin,
-			tmax,
-			0.0f,                        // rayTime
-			OptixVisibilityMask(1),
-			OPTIX_RAY_FLAG_TERMINATE_ON_FIRST_HIT,
-			RAY_TYPE_SHADOWRAY,         // SBT offset
-			RAY_TYPE_COUNT,             // SBT stride
-			RAY_TYPE_SHADOWRAY,      // missSBTIndex
-			occluded);
-	return occluded;
 }
